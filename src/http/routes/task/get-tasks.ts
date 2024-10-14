0b95c46a793c43f3bb3fb21b2dc91beec379d0c9
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-10-13 21:34:43 -0300

    Commit

--- src/http/routes/task/get-tasks.ts
+++ src/http/routes/task/get-tasks.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import z from "zod";
+import z, { number } from "zod";
+import { prisma } from "../../../lib/prisma";
 
 export async function getTasks(app: FastifyInstance) {
 
@@ -10,17 +11,36 @@ export async function getTasks(app: FastifyInstance) {
             summary: 'Get tasks',
             response: {
                 200: z.array(z.object({
-                    taskId: z.string().uuid(),
+                    idTask: z.string().uuid(),
+                    numberTask: z.number().nullable(),
                     title: z.string(),
                     description: z.string(),
-                    status: z.enum(['todo', 'doing', 'done']),
-                    userId: z.string(),
+                    userId: z.string().nullable(),
+                    typeTaskId: z.number(),
+                    subTypeTaskId: z.number(),
+                    idState: z.number(),
                 }))
             }
         }
     }, async (request, reply) => {
 
-        return reply.send()
+        const tasks = await prisma.task.findMany({
+            select: {
+                idTask: true,
+                numberTask: true,
+                title: true,
+                description: true,
+                userId: true,
+                typeTaskId: true,
+                subTypeTaskId: true,
+                idState: true,
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+
+        return reply.send(tasks)
 
 
 }
